37376191bbb8de576ecabaf663505fc160ac2c41
evtyuhov_roman <uchet-40@yandex.ru>
2026-01-25 14:43:55 +0300

    DZ 02

--- src/Lesson_02/moneyClass.java
+++ src/Lesson_02/moneyClass.java
@@ -9,51 +9,17 @@ public class moneyClass {
             String str = "";
             System.out.println("Введите число(1-99) :");
             money = scn.nextInt();
-            ed_money = money;
-
-           int  ost = money % 10;
+            int  ost = money % 10;
 
 
-
-            switch ((ost >= 1 && num <= 3) ? 2 :
-                (num >= 4 && num <= 6) ? 3 :
-                (num >= 7 && num <= 9) ? 4 :
-                (num >= 10 && num <= 12) ? 5 : 0) {
-            case 2 -> System.out.println(2);
-            case 3 -> System.out.println(3);
-            case 4 -> System.out.println(4);
-            case 5 -> System.out.println(5);
-            case 0 -> System.out.println("Такого значения не существует");
-        }
-
-
-
-
-
-
+                    switch ((( money < 10 || money > 20) && ost == 1 ) ? 1 : (ost >= 2 && ost <= 4) ? 2 : (ost >4) ? 3 :0)  {
+            case 1 -> System.out.println("копейка");
+            case 2 -> System.out.println("копейки");
+            case 3 -> System.out.println("копеек");
+            case 0 -> System.out.println("копеек");
+            }
             System.out.println("\n" + money + " " + str);
         }
     }
-
-
-/*            switch (ost) {
-                    case 1:
-                        str = "копейка";
-                        break;
-                    case 2:
-                        str = "копейки";
-                        break;
-                    case 3:
-                        str = "копейки";
-                        break;
-                    case 4:
-                        str = "копейки";
-                        break;
-                    default:
-                        str = "копеек";
-                        break;
-                }*/
-
-//     if(ed_money > 10 || ed_money < 20) str = "копеек";
 
 
