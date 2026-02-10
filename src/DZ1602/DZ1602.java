304ad7a9f0dce3f242e861ab73c34c62dce29adf
evtyuhov_roman <uchet-40@yandex.ru>
2026-02-10 15:45:34 +0300

    Домашнее задание 1602

---
+++ src/DZ1602/DZ1602.java
@@ -0,0 +1,39 @@
+package DZ1602;
+
+
+
+
+
+
+interface Operat{
+    int  execute();
+}
+
+public class DZ1602 {
+
+
+    static String str1 = "Лямбда выражение";
+    static String str2 = "Программа, которая подсчитывает количество слов в предложении";
+
+    public static void main(String[] args) {
+        String[] words1 = str1.split(" "); // массивы слов
+        String[] words2 = str2.split(" ");
+        String[][] nums = new String[2][];
+
+        nums[0] = words1;
+        nums[1] = words2;
+        Operat op = () -> {
+            for (int i = 0; i < nums.length; i++) {
+                int q = 0;
+                for (int j = 0; j < nums[i].length; j++) {
+                    int str2_ = nums[i][j].toCharArray().length;
+                    if (str2_ > 2) {q += 1;}    // исключаем предлоги
+                }
+                System.out.println("Количество слов в предложении равно   " + q);
+
+            };
+            return 0;
+        };
+        op.execute();
+    }
+}
