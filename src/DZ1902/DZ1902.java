6601c3623c9fe8846bfbb7f0312bb14c800978eb
evtyuhov_roman <uchet-40@yandex.ru>
2026-02-12 11:11:11 +0300

    Домашнее задание 1602 Доделал

--- src/DZ1902/DZ1902.java
+++ src/DZ1902/DZ1902.java
@@ -2,22 +2,28 @@ package DZ1902;
 
 import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Set;
 
 public class DZ1902 {
     static void main(String[] args) {
-        Integer nums[] =  { 2,5,2,7,8,8,10 };
-for(Integer n: nums)System.out.print(n+" ");
-
+        Integer nums[] = {2, 5, 2, 7, 8, 8, 10};
+        for (Integer n : nums) System.out.print(n + " ");
         System.out.println("\n");
-
-        Set<Integer> a = new HashSet<>();
-        a.addAll(Arrays.asList( nums));
-        System.out.println(a);
+        Set<Integer> a = new HashSet<Integer>();
+        a.addAll(Arrays.asList(nums));
+   //     System.out.println(a);
+        int arr[] = new int[a.size()];
+        Iterator<Integer> it = a.iterator();
+        int i=0;
+        int elt = 0;
+        for(int n: a) {
+            elt = it.next();
+            arr[i] =elt;
+             i++;
+        }
+        for(int j =0;j <= arr.length-1; j++ ){
+            System.out.print(arr[j]+" ");}
     }
-
-
-
-
-
 }
+
